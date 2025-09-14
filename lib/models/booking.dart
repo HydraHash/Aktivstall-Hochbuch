ea53d825b10bc0cbce477c43f3b58e0831001f21
HydraHash <patrickstrobel92@gmail.com>
2025-09-14 17:40:05 -0500

    Changed booking to week based

--- lib/models/booking.dart
+++ lib/models/booking.dart
@@ -2,25 +2,32 @@ class Booking {
   final int id;
   final int objectId;
   final int userId;
-  final DateTime startUtc;
-  final DateTime endUtc;
+  final DateTime start; // parsed DateTime (preserves timezone info)
+  final DateTime end;
+  final String? details;
 
   Booking({
     required this.id,
     required this.objectId,
     required this.userId,
-    required this.startUtc,
-    required this.endUtc,
+    required this.start,
+    required this.end,
+    this.details,
   });
 
   factory Booking.fromJson(Map<String, dynamic> j) {
-    DateTime parseDate(String? s) => s == null ? DateTime.fromMillisecondsSinceEpoch(0) : DateTime.parse(s);
+    DateTime parseDt(dynamic x) {
+      if (x == null) return DateTime.fromMillisecondsSinceEpoch(0);
+      return DateTime.parse(x as String);
+    }
+
     return Booking(
       id: j['id'] is int ? j['id'] : int.parse(j['id'].toString()),
-      objectId: j['object_id'] ?? j['objectId'] ?? 1,
-      userId: j['user_id'] ?? j['userId'] ?? 0,
-      startUtc: parseDate(j['start_time'] ?? j['startTime']),
-      endUtc: parseDate(j['end_time'] ?? j['endTime']),
+      objectId: j['object_id'] is int ? j['object_id'] : int.parse(j['object_id'].toString()),
+      userId: j['user_id'] is int ? j['user_id'] : int.parse(j['user_id'].toString()),
+      start: parseDt(j['start_time'] ?? j['startTime']),
+      end: parseDt(j['end_time'] ?? j['endTime']),
+      details: j['details'] as String?,
     );
   }
 }
