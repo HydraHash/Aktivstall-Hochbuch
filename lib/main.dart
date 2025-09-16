90f7a90f50d6ce881c1ffb6cb90bf3cbbff9d325
HydraHash <patrickstrobel92@gmail.com>
2025-09-16 15:21:36 -0500

    Add google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'screens/home_screen.dart';
 import 'screens/login_screen.dart';
 import 'services/auth_service.dart';
@@ -18,10 +19,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Booking App",
+      title: "Aktivstall Hochbuch",
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF8f7c74)),
         useMaterial3: true,
+        textTheme: GoogleFonts.interTextTheme(),
       ),
       home: FutureBuilder<Widget>(
         future: _decideStartPage(),
